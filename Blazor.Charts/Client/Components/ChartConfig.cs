a513efb89c8e79c994d73476697b6bf49bd3b4ec
Ben Dickman <Ben.Dickman@connellsgroup.co.uk>
2023-11-03 16:57:26 +0000

    update to get chart config and add pie chart

---
+++ Blazor.Charts/Client/Components/ChartConfig.cs
@@ -0,0 +1,81 @@
+﻿using static Blazor.Charts.Client.Components.Chart;
+
+namespace Blazor.Charts.Client.Components;
+
+public static class ChartConfig
+{
+    public static object GetBarChartConfig(
+        int[] data,
+        string[] backgroundColor,
+        string[] labels,
+        string title)
+    {
+        return new
+        {
+            Type = ChartType.Bar.ToString().ToLower(),
+            Options = new
+            {
+                Responsive = true,
+                Scales = new
+                {
+                    YAxes = new[]
+                    {
+                        new
+                        {
+                            Ticks = new
+                            {
+                                BeginAtZero=true
+                             }
+                        }
+                    }
+                }
+            },
+            Data = new
+            {
+                Datasets = new[]
+                {
+                    new
+                    {
+                        Data = data,
+                        BackgroundColor = backgroundColor,
+                        Label = title,
+                    }
+                },
+                Labels = labels
+            }
+        };
+    }
+
+    public static object GetPieChartConfig(
+        int[] data,
+        string[] backgroundColor,
+        string[] labels,
+        string title)
+    {
+        return new
+        {
+            Type = ChartType.Pie.ToString().ToLower(),
+            Options = new
+            {
+                Title = new
+                {
+                    Display = true,
+                    Text = title,
+                }
+            },
+            Data = new
+            {
+                Datasets = new[]
+                {
+                    new
+                    {
+                        Data = data,
+                        BackgroundColor = backgroundColor,
+                        Label = title,
+                    }
+                },
+                Labels = labels
+            }
+        };
+    }
+}
