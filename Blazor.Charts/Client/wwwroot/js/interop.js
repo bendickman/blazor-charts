f2b192cff35316833c17c7f65ec84b62030e7a93
Ben Dickman <Ben.Dickman@connellsgroup.co.uk>
2023-12-06 16:56:14 +0000

    call dotnet from js

--- Blazor.Charts/Client/wwwroot/js/interop.js
+++ Blazor.Charts/Client/wwwroot/js/interop.js
@@ -1,3 +1,25 @@
-﻿window.showAlert = function(message) {
+﻿window.showAlert = function (message) {
     alert(message);
 }
+
+window.callDotNetInstanceMethod = function (dotNetObjectReference) {
+    dotNetObjectReference.invokeMethodAsync("SetWindowSize",
+        {
+            width: window.innerWidth,
+            height: window.innerHeight,
+        });
+}
+
+window.registerResizeHandler = function (dotNetObjectReference) {
+    function resizeHandler() {
+        dotNetObjectReference.invokeMethodAsync("SetWindowSize",
+            {
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+    };
+
+    resizeHandler();
+
+    window.addEventListener("resize", resizeHandler);
+};
